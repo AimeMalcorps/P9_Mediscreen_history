c5ce5e192e6cd27dc33d86da7ab6c6fc4ae17d49
Aimé Malcorps <aime.malcorps@neuf.fr>
2023-06-22 17:04:13 +0200

    dependance Jacoco + tests controlleurs

--- src/test/java/com/mediscreen/history/notes/NotesTests.java
+++ src/test/java/com/mediscreen/history/notes/NotesTests.java
@@ -2,10 +2,10 @@ package com.mediscreen.history.notes;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,11 +25,10 @@ public class NotesTests {
 	NotesService notesService;
 	
 	@Test
-	@Order(1)
 	public void testAddNote() {
 		NoteDTO noteDTO = new NoteDTO();
 		noteDTO.setNote("Ceci est une note de test");
-		noteDTO.setPatientId(666);
+		noteDTO.setPatientId(1);
 		notesService.addNote(noteDTO);
 		
 		Note foundNote = null;
@@ -45,7 +44,6 @@ public class NotesTests {
 	}
 	
 	@Test
-	@Order(2)
 	public void testGetAllNotes() {
 		
 		List<Note> notesFromRepository = notesRepository.findAll();
@@ -57,58 +55,80 @@ public class NotesTests {
 	}
 	
 	@Test
-	@Order(3)
 	public void testGetNotesByPatientId() {
 		
-		List<NoteDTO> listNoteDTO = notesService.getNotesByPatientId(666);
+		Note note = new Note();
+		note.setId(222);
+		note.setNote("TEST");
+		note.setPatientId(1);
 		
-		NoteDTO foundNoteDTO = null;
+		List<NoteDTO> listNoteDTO = notesService.getNotesByPatientId(222);
 		
-		if (listNoteDTO.size() == 1) {
-			foundNoteDTO = listNoteDTO.get(0);
-		} else {
-			foundNoteDTO = listNoteDTO.stream()
-			  .filter(note -> note.getNote().equals("Ceci est une note de test"))
-			  .findAny()
-			  .orElse(null);
+		boolean notesForSamePatient = true;
+		
+		for (NoteDTO noteDTO : listNoteDTO) {
+			if (noteDTO.getPatientId() != 222) {
+				notesForSamePatient = false;
+			}
 		}
-		
-		assertNotNull(foundNoteDTO);
+		assertTrue(notesForSamePatient);
 	}
 	
 	@Test
-	@Order(4)
 	public void testModifyNote() {
 		
-		NoteDTO noteDTO = new NoteDTO();
-		noteDTO.setNote("Old Note");
-		noteDTO.setPatientId(1);
+		Note note = new Note();
+		note.setId(22);
+		note.setNote("Old Note");
+		note.setPatientId(1);
 		
-		notesRepository.save(new Note(noteDTO, 0));
+		notesRepository.save(note);
 		
 		String newNote = "Modified note";
 		
-		Note note = notesRepository.findById(1).get();
+		note = notesRepository.findById(22).get();
 		
-		noteDTO.setNote(newNote);
-		noteDTO.setPatientId(note.getPatientId());
+		note.setNote(newNote);
+		
+		NoteDTO noteDTO = new NoteDTO(note);
 		
 		notesService.modifyNote(noteDTO);
 			
-		assertEquals(newNote, notesRepository.findById(1).get().getNote());	
+		assertEquals(newNote, notesRepository.findById(22).get().getNote());	
 	}
 	
 	@Test
-	@Order(5)
 	public void testDeleteNote() {
+		Note note = new Note();
+		note.setId(11);
+		note.setPatientId(11);
+		note.setNote("Test");
+		notesRepository.save(note);
 		
 		int bataBaseSize = notesRepository.findAll().size();
 		
-		notesService.deleteNote(1);
+		notesService.deleteNote(11);
 		
 		int newDataBaseSize = notesRepository.findAll().size();
 		
-		assertEquals(bataBaseSize, newDataBaseSize + 1);
+		assertEquals(newDataBaseSize, bataBaseSize - 1);
+	}
+	
+	@Test
+	public void testDeleteAllNote() {
+		
+		Note note = new Note();
+		note.setPatientId(12);
+		note.setNote("Test");
+		notesRepository.save(note);
+		
+		int patientNotesSize = notesRepository.findAll().size();
+		
+		notesService.deleteAllPatientNotes(12);
+		
+		int newDataBaseSize = notesRepository.findAll().size();
+		
+		assertEquals(newDataBaseSize, patientNotesSize - 1);
 	}
 
 }
